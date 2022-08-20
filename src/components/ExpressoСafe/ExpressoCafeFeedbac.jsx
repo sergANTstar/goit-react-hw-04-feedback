be6afc72eb6280ae08d79109d6bdd2b216348742
sergANTstar <106014778+sergANTstar@users.noreply.github.com>
2022-08-20 15:52:56 +0300

    г

--- src/components/ExpressoСafe/ExpressoCafeFeedbac.jsx
+++ src/components/ExpressoСafe/ExpressoCafeFeedbac.jsx
@@ -6,12 +6,12 @@ import {Statistics} from '../Statistics/Statistics';
 
 function ExpressoCafeFeedbac() {
   
-    const [good, setGood] = useState(0);
-    const [neutral, setNeutral] = useState(0);
-    const [ bad, setBad] = useState(0);
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
-    const options = ['good', 'neutral', 'bad'];
-
+  const options = ['good', 'neutral', 'bad'];
+  
     const totalFeedback =() => {
         return good + neutral + bad;
     };
